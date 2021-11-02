03692c871413da33c84084818b10f6b946bfd772
Aryan Mahesh <aryan.mahesh2010@gmail.com>
2021-11-02 23:24:01 +0530

    *

--- audio_recognition/onesecondchunks.py
+++ audio_recognition/onesecondchunks.py
@@ -3,7 +3,7 @@ import sys
 import wave
 
 chunk = 4096
-FORMAT = pyaudio.paInt32
+FORMAT = pyaudio.paFloat32
 CHANNELS = 1
 RATE = 16000
 RECORD_SECONDS = 3
@@ -18,21 +18,21 @@ stream = p.open(format=FORMAT,
                 input = True, 
                 frames_per_buffer = chunk, 
                 input_device_index = 1)
+frames = []
 
-for i in range(0, NFRAMES):
+for i in range(0, int(NFRAMES / 2)):
     data = stream.read(chunk, exception_on_overflow = False)
+    frames.append(data)
 
-    try:
-        while True:
-            print("Listening...")
-            frames = []
-            for i in range(0, NFRAMES):
-                data = stream.read(chunk, exception_on_overflow = False)
-                frames.append(data)
-    except KeyboardInterrupt:
-        print("Program terminated")
-    
-    
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+
+print("Program terminated")
+stream.stop_stream()
+stream.close()
+p.terminate()
+# Save the recorded data as a WAV file
+wf = wave.open("output.wav", 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
